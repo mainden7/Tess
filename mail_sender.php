b3d8df7eb3f1cc622a135cb94a674ce91a22a8ed
Denis <racerx20001@gmail.com>
2016-11-01 13:30:54 +0300

    mail send fix#2

--- mail_sender.php
+++ mail_sender.php
@@ -1,9 +1,11 @@
 <?php
 if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")&&(empty($_POST['spam_hidden']))){ 
-        $to = 'denis.sa@core-tech.ru';
+        $to = 'klmv-s@yandex.ru';
         $subject = 'Обратный звонок';
         if(isset($_POST['comment'])&&!empty($_POST['comment'])){
             $msg = $_POST['comment'];
+        }else{
+            $msg = '';
         }        
         $message = '
                 <html>
@@ -13,7 +15,7 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['t
                     <body>
                         <p>Имя: '.$_POST['name'].'</p>
                         <p>Телефон: '.$_POST['phone'].'</p>  
-                        <p>Телефон: '.(isset($msg) ? $msg : '').'</p>                      
+                        <p>Сообщение: '.$msg.'</p>                      
                     </body>
                 </html>'; 
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
